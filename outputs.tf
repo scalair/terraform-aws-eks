d12f7b26a895029e30edbb022299847d9e17661b
Adel Abdelhak <6298183+blakelead@users.noreply.github.com>
2020-01-13 16:38:12 +0100

    Add output 'cluster_oidc_issuer_url'

--- outputs.tf
+++ outputs.tf
@@ -5,6 +5,10 @@ output "cluster_id" {
   value = module.eks.cluster_id
 }
 
+output "cluster_oidc_issuer_url" {
+  value       = module.eks.cluster_oidc_issuer_url
+}
+
 output "cluster_arn" {
   value = module.eks.cluster_arn
 }
