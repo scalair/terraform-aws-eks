ba7e9316ad4a877e536fcca1a48d30a7c70301bb
Xat59 <descamps.jeremy@gmail.com>
2020-11-18 15:40:36 +0100

    lint: terraform fmt

--- main.tf
+++ main.tf
@@ -1,56 +1,56 @@
 locals {
-	iam_user_name = var.iam_user_name == "" ? "admin_${var.cluster_name}" : var.iam_user_name 
+  iam_user_name = var.iam_user_name == "" ? "admin_${var.cluster_name}" : var.iam_user_name
 }
 
 module "iam_user" {
-	source = "github.com/terraform-aws-modules/terraform-aws-iam//modules/iam-user?ref=v3.4.0"
+  source = "github.com/terraform-aws-modules/terraform-aws-iam//modules/iam-user?ref=v3.4.0"
 
-	name				= local.iam_user_name
-	pgp_key       		= var.iam_user_pgp_key
-	force_destroy 		= var.iam_user_force_destroy
-	create_user   		= var.iam_user_create_user
+  name          = local.iam_user_name
+  pgp_key       = var.iam_user_pgp_key
+  force_destroy = var.iam_user_force_destroy
+  create_user   = var.iam_user_create_user
 
-	tags				= var.tags
+  tags = var.tags
 }
 
 # Admin policies to access cluster
 resource "aws_iam_user_policy_attachment" "AmazonEKSServicePolicy" {
-	count = var.iam_user_create_user ? 1 : 0
+  count = var.iam_user_create_user ? 1 : 0
 
-	user       = module.iam_user.this_iam_user_name
-	policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
+  user       = module.iam_user.this_iam_user_name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSServicePolicy"
 }
 
 resource "aws_iam_user_policy_attachment" "AmazonEKSClusterPolicy" {
-	count = var.iam_user_create_user ? 1 : 0
+  count = var.iam_user_create_user ? 1 : 0
 
-	user       = module.iam_user.this_iam_user_name
-	policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  user       = module.iam_user.this_iam_user_name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
 resource "aws_iam_user_policy_attachment" "AmazonEKSWorkerNodePolicy" {
-	count = var.iam_user_create_user ? 1 : 0
+  count = var.iam_user_create_user ? 1 : 0
 
-	user       = module.iam_user.this_iam_user_name
-	policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  user       = module.iam_user.this_iam_user_name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
 module "eks" {
-	source          = "terraform-aws-modules/eks/aws"
-	cluster_name    = var.cluster_name
-	cluster_version = var.cluster_version
-	subnets         = var.subnets
-	vpc_id          = var.vpc_id
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+  subnets         = var.subnets
+  vpc_id          = var.vpc_id
 
-	worker_groups 	= var.worker_groups
+  worker_groups = var.worker_groups
 
-	map_users = [
-	{
-		userarn  = module.iam_user.this_iam_user_arn
-		username = module.iam_user.this_iam_user_name
-		groups   = ["system:masters"]
-	}
-	]
+  map_users = [
+    {
+      userarn  = module.iam_user.this_iam_user_arn
+      username = module.iam_user.this_iam_user_name
+      groups   = ["system:masters"]
+    }
+  ]
 
-	tags			= var.tags
+  tags = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,66 +1,66 @@
 variable "cluster_name" {
-    description = "Name of the EKS cluster. Also used as a prefix in names of related resources."
-    type        = string
+  description = "Name of the EKS cluster. Also used as a prefix in names of related resources."
+  type        = string
 }
 
 variable "cluster_version" {
-    description = "Kubernetes version to use for the EKS cluster."
-    type        = string
+  description = "Kubernetes version to use for the EKS cluster."
+  type        = string
 }
 
 variable "subnets" {
-    description = "A list of subnets to place the EKS cluster and workers within."
-    type        = list(string)
+  description = "A list of subnets to place the EKS cluster and workers within."
+  type        = list(string)
 }
 
 variable "vpc_id" {
-    description = "VPC where the cluster and workers will be deployed."
-    type        = string
+  description = "VPC where the cluster and workers will be deployed."
+  type        = string
 }
 
 variable "worker_groups" {
-    description = "A list of maps defining worker group configurations to be defined using AWS Launch Configurations. See workers_group_defaults for valid keys."
-    type        = any
-    default     = []
+  description = "A list of maps defining worker group configurations to be defined using AWS Launch Configurations. See workers_group_defaults for valid keys."
+  type        = any
+  default     = []
 }
 
 variable "map_roles" {
-    description = "Additional IAM roles to add to the aws-auth configmap. See examples/basic/variables.tf for example format."
-    type = list(object({
+  description = "Additional IAM roles to add to the aws-auth configmap. See examples/basic/variables.tf for example format."
+  type = list(object({
     rolearn  = string
     username = string
     groups   = list(string)
-    }))
-    default = []
+  }))
+  default = []
 }
 
 variable "iam_user_pgp_key" {
-    description = "Either a base-64 encoded PGP public key, or a keybase username in the form keybase:username. Used to encrypt password and access key."
-    type        = string
-    default     = ""
+  description = "Either a base-64 encoded PGP public key, or a keybase username in the form keybase:username. Used to encrypt password and access key."
+  type        = string
+  default     = ""
 }
 
 variable "iam_user_name" {
-    description = "Desired name for the IAM user"
-    type        = string
-    default     = ""
+  description = "Desired name for the IAM user"
+  type        = string
+  default     = ""
 }
 
 variable "iam_user_force_destroy" {
-    description = "When destroying this user, destroy even if it has non-Terraform-managed IAM access keys, login profile or MFA devices. Without force_destroy a user with non-Terraform-managed access keys and login profile will fail to be destroyed."
-    type        = bool
-    default     = true
+  description = "When destroying this user, destroy even if it has non-Terraform-managed IAM access keys, login profile or MFA devices. Without force_destroy a user with non-Terraform-managed access keys and login profile will fail to be destroyed."
+  type        = bool
+  default     = true
 }
 
 variable "iam_user_create_user" {
-    description = "Whether to create the IAM user"
-    type        = bool
-    default     = true
+  description = "Whether to create the IAM user"
+  type        = bool
+  default     = true
 }
 
 variable "tags" {
-    description = "A map of tags to add to all resources. Tags added to launch coniguration or templates override these values for ASG Tags only."
-    type        = map(string)
-    default     = {}
+  description = "A map of tags to add to all resources. Tags added to launch coniguration or templates override these values for ASG Tags only."
+  type        = map(string)
+  default     = {}
 }
 
